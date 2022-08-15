b3ae0aced4f1423412688dd2811714d13b79a806
vitoriajaco <vitoriajaco@gmail.com>
2022-08-15 18:47:10 -0300

    Iniciando com Design Patterns

---
+++ src/CalculadoraDeImpostos.java
@@ -0,0 +1,23 @@
+import TipoImposto.TipoImposto;
+
+import java.math.BigDecimal;
+
+import static TipoImposto.TipoImposto.ICMS;
+import static TipoImposto.TipoImposto.ISS;
+
+public class CalculadoraDeImpostos {
+
+    public BigDecimal calcular(Orcamento orcamento, TipoImposto tipoImposto){
+
+        switch (tipoImposto) {
+            case ICMS:
+                return orcamento.getValor().multiply(new BigDecimal("0.1"));
+            case ISS:
+                return orcamento.getValor().multiply(new BigDecimal(0.06));
+            default:
+                return BigDecimal.ZERO;
+
+        }
+
+    }
+}
---
+++ src/TipoImposto/TipoImposto.java
@@ -0,0 +1,7 @@
+package TipoImposto;
+
+public enum TipoImposto {
+
+    ICMS,
+    ISS,
+}
