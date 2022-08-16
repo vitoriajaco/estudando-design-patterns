a999de982b55c67353f65a7eb5d0a8911db31bf3
vitoriajaco <vitoriajaco@gmail.com>
2022-08-16 09:18:53 -0300

    Aplicando strategy

--- src/TestesImpostos.java
+++ src/TestesImpostos.java
@@ -1,11 +1,9 @@
-import TipoImposto.TipoImposto;
-
 import java.math.BigDecimal;
 
 public class TestesImpostos {
     public static void main(String[] args) {
         Orcamento orcamento = new Orcamento(new BigDecimal("100"));
         CalculadoraDeImpostos calculadora = new CalculadoraDeImpostos();
-        System.out.println(calculadora.calcular(orcamento, TipoImposto.ISS));
+        System.out.println(calculadora.calcular(orcamento, new ISS()));
     }
 }
